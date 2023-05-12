d88652fed4970f085ae45b18b5b3e962a980f6d7
Nahuel Rodriguez <nahufed@gmail.com>
2023-05-12 00:25:14 -0300

    terminando logica create

--- js/administrador.js
+++ js/administrador.js
@@ -46,55 +46,17 @@ console.log(listaVideojuegos);
 
 // manejadores de eventos
 formularioVideojuego.addEventListener("submit", prepararFormulario);
-btnAgregarVideojuego.addEventListener('click', mostrarModalVideojuego);
 
-cargaInicial();
 
-//funciones
-function cargaInicial() {
-  if (listaVideojuegos.length > 0) {
-    //dibujar las filas de la tabla
-    listaVideojuegos.map((Videojuego, indice) => crearFila(Videojuego, indice + 1));
-  }
-  //le muestro el msj que no tengo elementos
-}
 
-function crearFila(Videojuego, indice) {
-  let tablaVideojuego = document.querySelector("tbody");
-  tablaVideojuego.innerHTML += `              <tr>
-  <th scope="row">${indice}</th>
-  <td>${Videojuego.nombre}</td>
-  <td>${Videojuego.precio}</td>
-  <td>${Videojuego.categoria}</td>
-  <td> <span class="d-inline-block text-truncate truncarTexto">
-  ${Videojuego.imagen}
-  </span></td>
-  <td><span class="d-inline-block text-truncate truncarTexto">
-  ${Videojuego.descripcion}
-  </span></td>
-  <td><span class="d-inline-block text-truncate truncarTexto">
-  ${Videojuego.requisitos}
-  </span></td>
-  <td>${Videojuego.desarrollador}</td>
-  <td>${Videojuego.reseña}</td>
-  <td>
-    <button type="button" class="btn btn-outline-warning mb-1" data-bs-toggle="modal" data-bs-target="#adminModal">
-      <i class="bi bi-pencil-fill"></i>
-    </button>
-    <button type="button" class="btn btn-outline-danger">
-      <i class="bi bi-trash3"></i>
-    </button>
-  </td>
-</tr>`;
-}
+
+
 
 function prepararFormulario(e) {
   e.preventDefault();
-  if(verificarCrearVideojuego){
-    crearVideojuego();
-  }else{
-    editarVideojuego();
-  }
+
+  crearVideojuego();
+
 }
 
 function crearVideojuego() {
@@ -109,16 +71,14 @@ function crearVideojuego() {
   if (resumeErrores.length === 0) {
     //creo  el videojuego
     mostrarAlert(false, "");
-    let nuevoVideojuego = new Videojuego(
-      undefined,
+    let nuevoVideojuego = new Videojuego(      
       nombre.value,
       precio.value,
-      categoria.value,
-      imagen.value,
+      categoria.value,      
       imagen.value,
       descripcion.value,
-      desarrollador.value,
       requisitos.value,      
+      desarrollador.value,
       reseñas.value      
     );
     console.log(nuevoVideojuego);
@@ -136,16 +96,13 @@ function crearVideojuego() {
       "success"
     );
     //dibuja la fila
-    crearFila(nuevoVideojuego, listaVideojuegos.length);
+
   } else {
     //falla la validacion
     mostrarAlert(true, resumeErrores);
   }
 }
 
-function guardarEnLocalstorage() {
-  localStorage.setItem("listaVideojuegos", JSON.stringify(listaVideojuegos));
-}
 
 function mostrarAlert(estado, resumeErrores) {
   //estado = true muestro el alert, caso contrario oculto
